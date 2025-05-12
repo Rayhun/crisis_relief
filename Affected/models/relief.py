0a49a80b19ad64ddf00f01390045b2c3afc65b26
Md Rayhan <rayhunkhan27@gmail.com>
2025-05-13 02:29:26 +0600

    add releafe

--- Affected/models/relief.py
+++ Affected/models/relief.py
@@ -20,6 +20,9 @@ STATUS_CHOICES = [
 
 class ReliefRequest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        'Event', on_delete=models.CASCADE, related_name='relief_requests'
+    )
     title = models.CharField(max_length=100)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
